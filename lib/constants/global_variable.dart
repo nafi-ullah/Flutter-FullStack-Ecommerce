9972565211a0b9cf3f990d6db0780260e1a55e70
nafi-ullah <shafinnafiullah@gmail.com>
2023-11-08 18:11:17 +0600

    authscreen and route setting

--- lib/constants/global_variable.dart
+++ lib/constants/global_variable.dart
@@ -11,7 +11,7 @@ class GlobalVariable{
   );
 
   static const secondaryColor = Color.fromRGBO(255, 153, 0, 1);
-  static const backgroundColor = Colors.blueAccent;
+  static const backgroundColor = Colors.white;
   static const Color greyBackgroundCOlor = Color(0xffebecee);
   static var selectedNavBarColor = Colors.cyan[800]!;
   static const unselectedNavBarColor = Colors.black87;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:amazon_clone_app/constants/global_variable.dart';
+import 'package:amazon_clone_app/features/auth/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:amazon_clone_app/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +17,38 @@ class MyApp extends StatelessWidget {
       title: 'Amazon Clone',
       theme: ThemeData(
         scaffoldBackgroundColor: GlobalVariable.backgroundColor,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black)
+        ),
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariable.secondaryColor,
+
+        ),
         useMaterial3: true,
       ),
+      onGenerateRoute: (settings) => generateRoute(settings),
       home: Scaffold(
-        appBar: AppBar(title: Text("Amazon")),
-        body: const Text(
-          'Flutter Demo Home Page')
+        appBar: AppBar(
+          title: const Text("Amazon"),
+          backgroundColor: Colors.lightBlue,
+          ),
+        body:  Center(
+          child: Column(
+            children: [
+               const Text(
+                'Flutter Demo Home Page'),
+                Builder(
+                  builder: (context) {
+                    return ElevatedButton(onPressed: (){
+
+                      Navigator.pushNamed(context, AuthScreen.routeName);
+                    }, child: const Text("data"));
+                  }
+                ),
+            ],
+          ),
+        )
           ),
     );
   }
